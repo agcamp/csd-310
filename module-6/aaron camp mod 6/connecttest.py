87d277517042d6114dfb0708c1fb95aca19fd742
agcamp <acamp@my365.bellevue.edu>
2024-07-02 16:21:34 -0500

    mod 6

---
+++ module-6/aaron camp mod 6/connecttest.py
@@ -0,0 +1,22 @@
+
+
+import mysql.connector
+from mysql.connector import errorcode
+
+from mysql_test import config
+
+try:
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+finally:
+    db.close()
+
---
+++ module-6/aaron camp mod 6/mysql_test.py
@@ -0,0 +1,8 @@
+
+config = {
+    "user": "root",
+    "password": "cats81Craddle",
+    "host": "127.0.0.1",
+    "database": "movies",
+    "raise_on_warnings": True
+}
